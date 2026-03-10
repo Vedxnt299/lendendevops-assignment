916cdfd8919fec9872776261486ec7fc6f225eeb
Vedant Nimbalkar <vedantnimbalkar299@gmail.com>
2026-03-10 23:58:39 +0530

    Updated Node.js App

--- app/index.js
+++ app/index.js
@@ -2,7 +2,15 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res)=>{
-res.send("LenDenClub DevOps Assignment Running")
+res.send(`
+      <h1>LenDenClub DevOps Assignment</h1>
+      <p>Application deployed using:</p>
+      <ul>
+         <li>Terraform Infrastructure as Code</li>
+         <li>Jenkins CI/CD Pipeline</li>
+         <li>Trivy Security Scanning</li>
+      </ul>
+    `)
 })
 
 app.listen(3000, ()=>{
