6c767143550ceb08fc6e30aa1ff2805eb55fb80e
Vedant Nimbalkar <vedantnimbalkar299@gmail.com>
2026-03-10 19:17:42 +0530

    Fix security group vulnerability detected by Trivy

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,16 +1,11 @@
 resource "aws_security_group" "web_sg" {
-  name = "devops-assignment-sg"
+  name        = "devops-assignment-sg"
+  description = "Allow HTTP traffic"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
